095196b4d02d3ca3f27e1ed9a5f6b21d77a0cad0
andGuan <ggorvi@gmail.com>
2026-09-15 14:15:54 +0800

    Carslice update

--- src/CartSlice.jsx
+++ src/CartSlice.jsx
@@ -1,23 +1,37 @@
 import { createSlice } from '@reduxjs/toolkit';
 
-export const CartSlice = createSlice({
-  name: 'cart',
-  initialState: {
-    items: [], // Initialize items as an empty array
-  },
+const initialState = {
+  items: []
+};
+
+const cartSlice = createSlice({
+  name: "cart",
+  initialState,
   reducers: {
     addItem: (state, action) => {
-    
+      const { name, image, cost } = action.payload;
+      const existingItem = state.items.find(item => item.name === name);
+      if(existingItem){
+        existingItem.quantity++;
+      }else{
+        state.items.push({ name, image, cost, quantity:1 })
+      }
     },
     removeItem: (state, action) => {
+      // payload传商品name
+      state.items = state.items.filter(item => item.name !== action.payload);
     },
     updateQuantity: (state, action) => {
+      const { name, quantity } = action.payload;
+      const itemToUpdate = state.items.find(item => item.name === name);
+      if(itemToUpdate){
+        itemToUpdate.quantity = quantity;
+      }
+    }
+  }
+})
 
-    
-    },
-  },
-});
-
-export const { addItem, removeItem, updateQuantity } = CartSlice.actions;
-
-export default CartSlice.reducer;
+// 导出action，组件中使用
+export const { addItem, removeItem, updateQuantity } = cartSlice.actions;
+// 默认导出reducer，供store.js导入
+export default cartSlice.reducer;
